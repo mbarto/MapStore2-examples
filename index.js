f81fda6c8bffe38371d6996fa0c7f8a9fc5e8e3a
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2018-07-29 20:10:44 +0200

    Added gzip compression

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const compression = require('compression');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+app.use(compression());
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
